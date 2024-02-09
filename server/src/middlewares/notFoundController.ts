34177f443d4eba640a81be3f0de732bbe67bd2a4
stupot2dstudy <stupot2dstudy@gmail.com>
2024-02-09 15:51:07 +0100

    thrid commit

--- server/src/middlewares/notFoundController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'; // Importa los tipos Request y Response de Express para manejar las solicitudes y respuestas HTTP.
-
-// Define el controlador notFoundController que maneja solicitudes para rutas no encontradas.
-export const notFoundController = (req: Request, res: Response) => {
-    // Envía una respuesta con estado 404 y un mensaje indicando "Not Found".
-    res.status(404).json({ message: 'No encontrado' });
-};
--- server/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-// Importa y exporta todas las funciones de los modelos
-export { createTaskModel } from './newTaskModel';
-export { deleteTaskModel } from './deleteTaskModel';
-export { getAllTasksModel } from './getAllTaskModel';
-export { getTaskByIdModel } from './getTaskByIdModel';
-export { updateTaskModel } from './updateTaskModel';
